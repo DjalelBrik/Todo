c20a71dd370b7d0118e381cd31bcd974b6a66187
DjalelBrik <lpc0552@gmail.com>
2026-03-13 14:38:59 +0000

    Add the localStorage

--- todo/src/Context/TaskContext.jsx
+++ todo/src/Context/TaskContext.jsx
@@ -1,8 +1,19 @@
-import { createContext,useState } from "react";
+import { createContext,useState,useEffect } from "react";
 
 export const TaskContext=createContext([]);
 export function TaskProvider({children}){
-    const [task,settask]=useState([]);
+const [task, settask] = useState(() => {
+    const savedtask = localStorage.getItem("task");
+    if (!savedtask) return [];
+    try {
+      return JSON.parse(savedtask);
+    } catch {
+      return [];
+    }
+  });
+    useEffect(() => {
+    localStorage.setItem("task", JSON.stringify(task));
+  }, [task]);
     return(
         <TaskContext.Provider value={{task,settask}}>
             {children}
